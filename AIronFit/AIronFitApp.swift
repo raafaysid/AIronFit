69d85cb47688c1e46ac66c08b86012630536075c
raafaysid <raafaysid@gmail.com>
2026-03-19 00:27:48 -0500

    Initial Commit

---
+++ AIronFit/AIronFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIronFitApp.swift
+//  AIronFit
+//
+//  Created by Raafay Siddiqui on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AIronFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
